6d37efa4c6242931059657a322ce9cf407e517e9
Tony Bowden <tony@tmtm.com>
2021-07-16 12:34:13 +0300

    initial scrapers and data

---
+++ bin/scraper/official.rb
@@ -0,0 +1,36 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'open-uri/cached'
+require 'pry'
+
+# TODO: allow ScraperData to use Cabinet here!
+class Legislature
+  # details for an individual member
+  class Member < Scraped::HTML
+    field :name do
+      noko.text.tidy
+    end
+
+    field :position do
+      noko.xpath('following-sibling::p').text.tidy
+    end
+  end
+
+  # The page listing all the members
+  class Members < Scraped::HTML
+    field :members do
+      container.map { |member| fragment(member => Member).to_h }
+    end
+
+    private
+
+    def container
+      noko.css('.list-with-image-vp .font-weight-bold')
+    end
+  end
+end
+
+url = 'https://www.valitsus.ee/peaminister-ministrid/valitsuse-koosseis'
+puts EveryPoliticianScraper::ScraperData.new(url).csv
