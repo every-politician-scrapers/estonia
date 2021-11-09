433c4348b7ae4cd673e589c6bffdca4a33c5dd09
Tony Bowden <tony@tmtm.com>
2021-11-09 21:24:39 +0200

    switch to latest html build

--- pcb/generate-html.sh
+++ pcb/generate-html.sh
@@ -8,14 +8,14 @@ UNIQUE_BIOS=$(mktemp)
 # Current holders of each wanted position
 qsv select position wikidata/wanted-positions.csv |
   qsv behead |
-  xargs wd sparql bin/holders.js -f csv > $TMPFILE
+  xargs wd sparql pcb/holders.js -f csv > $TMPFILE
 sed -e 's#http://www.wikidata.org/entity/##g' -e 's/T00:00:00Z//g' $TMPFILE > $HOLDERS
 
 # Biographical info for current officeholders
 qsv select person $HOLDERS |
   qsv dedup |
   qsv behead |
-  xargs wd sparql bin/bios.js -f csv > $TMPFILE
+  xargs wd sparql pcb/bios.js -f csv > $TMPFILE
 sed -e 's#http://www.wikidata.org/entity/##g' -e 's/T00:00:00Z//g' $TMPFILE > $SORTED_BIOS
 
 # Remove (and report on) extraneous bio info
@@ -25,9 +25,11 @@ qsv dedup -s person -D wikidata/results/extraneous-bios.csv $SORTED_BIOS > $UNIQ
 qsv join position wikidata/wanted-positions.csv position $HOLDERS |
   qsv select position,title,person,start > $TMPFILE
 qsv join person $TMPFILE person $UNIQUE_BIOS |
-  qsv select title,start,person,personLabel,genderLabel,dob,dobPrecision,dod,dodPrecision,image |
-  ifne tee html/current.csv
+  qsv select title,personLabel,person,start,genderLabel,dob,dobPrecision,dod,dodPrecision,image |
+  qsv rename position,person,personID,start,gender,DOB,dobp,DOD,dodp,image > html/current.csv
 
 # Generate HTML
-erb country=$(jq -r .jurisdiction.name meta.json) csvfile=html/current.csv -r csv -T- template/index.erb |
-  ifne tee html/index.html
+erb country="$(jq -r .jurisdiction.name meta.json)" csvfile=html/current.csv -r csv -T- pcb/index.erb > html/index.html
+
+echo "No matches for:"
+qsv join --left-anti title wikidata/wanted-positions.csv position html/current.csv
