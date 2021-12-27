8df62139e03d5cdc2b236cc85d989b4ce4012b8a
Tony Bowden <tony@tmtm.com>
2021-12-27 12:51:57 +0200

    add current-cabinet sparql

--- wikidata/queries/current-cabinet.js
+++ wikidata/queries/current-cabinet.js
@@ -3,37 +3,27 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
-  return `SELECT DISTINCT (STRAFTER(STR(?item), STR(wd:)) AS ?wdid)
-               ?name ?wdLabel ?source ?sourceDate
-               (STRAFTER(STR(?positionItem), STR(wd:)) AS ?pid) ?position
+  return `SELECT DISTINCT ?item ?itemLabel ?position ?positionLabel ?start
                (STRAFTER(STR(?held), '/statement/') AS ?psid)
         WHERE {
           # Positions currently in the cabinet
-          ?positionItem p:P361 ?ps .
+          ?position p:P361 ?ps .
           ?ps ps:P361 wd:${meta.cabinet.parent} .
           FILTER NOT EXISTS { ?ps pq:P582 [] }
 
           # Who currently holds those positions
           ?item wdt:P31 wd:Q5 ; p:P39 ?held .
-          ?held ps:P39 ?positionItem ; pq:P580 ?start .
+          FILTER NOT EXISTS { ?item wdt:P570 [] }
+
+          ?held ps:P39 ?position ; pq:P580 ?start .
+          OPTIONAL { ?held pq:P582 ?end }
+
           FILTER NOT EXISTS { ?held wikibase:rank wikibase:DeprecatedRank }
-          FILTER NOT EXISTS { ?held pq:P582 [] }
+          FILTER (?start < NOW())
+          FILTER (!BOUND(?end) || ?end > NOW())
 
-          OPTIONAL {
-            ?held prov:wasDerivedFrom ?ref .
-            ?ref pr:P854 ?source FILTER CONTAINS(STR(?source), '${meta.cabinet.source.url}') .
-
-            OPTIONAL { ?ref pr:P1810 ?sourceName }
-            OPTIONAL { ?ref pr:P1932 ?statedName }
-            OPTIONAL { ?ref pr:P813  ?sourceDate }
-          }
-
-          OPTIONAL { ?item rdfs:label ?wdLabel FILTER(LANG(?wdLabel) = "${meta.cabinet.source.lang.code}") }
-          BIND(COALESCE(?sourceName, ?wdLabel) AS ?name)
-
-          OPTIONAL { ?positionItem wdt:P1705  ?nativeLabel   FILTER(LANG(?nativeLabel)   = "${meta.cabinet.source.lang.code}") }
-          OPTIONAL { ?positionItem rdfs:label ?positionLabel FILTER(LANG(?positionLabel) = "${meta.cabinet.source.lang.code}") }
-          BIND(COALESCE(?statedName, ?nativeLabel, ?positionLabel) AS ?position)
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
         }
-        ORDER BY STR(?name) STR(?position) ?began ?wdid ?sourceDate`
+        # ${new Date().toISOString()}
+        ORDER BY ?item ?position ?start ?psid`
 }
